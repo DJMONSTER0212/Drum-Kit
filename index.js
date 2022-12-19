74334b70fdd91c57d5a5be7f4ec75134bb3ae042
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2022-12-19 20:52:51 +0530

    organising our code

--- index.js
+++ index.js
@@ -7,38 +7,7 @@ for(let i =0;i<document.querySelectorAll('button').length;i++){
     // document.querySelectorAll('button')[i].addEventListener('click',handleClick) //we don't use parenthesis in function call bcz if we used it then the function will be directly called  || paranthesis used to directly call the function   
     document.querySelectorAll('button')[i].addEventListener('click',function(){
         var buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
-            case 'w':
-                var audio = new Audio('sounds/tom-1.mp3')
-                audio.play();
-                break;  
-            case 'a':
-                var audio = new Audio('sounds/tom-2.mp3')
-                audio.play();
-                break;
-            case 's':
-                var audio = new Audio('sounds/tom-3.mp3')
-                audio.play();
-                break;
-            case 'd':
-                var audio = new Audio('sounds/tom-4.mp3')
-                audio.play();
-                break;
-            case 'j':
-                var audio = new Audio('sounds/kick-bass.mp3')
-                audio.play();
-                break;
-            case 'k':
-                var audio = new Audio('sounds/crash.mp3')
-                audio.play();
-                break;
-            case 'l':
-                var audio = new Audio('sounds/snare.mp3')
-                audio.play();
-                break;
-            default: console.log(this.innerHTML)
-
-        }
+        makeSound(buttonInnerHTML)
     })
     
 }
@@ -46,7 +15,10 @@ for(let i =0;i<document.querySelectorAll('button').length;i++){
 //the event parameter will let us to tap in to the event that triggered the function
 document.addEventListener('keypress',function(event){ //keyboard event listner is added in the whole document so that the whole document can hadle key press || first event is generated then the function gets triggered
     var keypressed = event.key;
-    switch (keypressed) {
+    makeSound(keypressed)
+})
+function makeSound(key){
+    switch (key) {
         case 'w':
             var audio = new Audio('sounds/tom-1.mp3')
             audio.play();
@@ -78,4 +50,4 @@ document.addEventListener('keypress',function(event){ //keyboard event listner i
         default: console.log(event.key)
 
     }
-})
+}
