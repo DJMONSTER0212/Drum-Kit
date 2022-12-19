bfca693b2cc993ec53b36768aeb8f333e3628747
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2022-12-19 20:26:31 +0530

    added keyboard event listner

--- index.js
+++ index.js
@@ -42,3 +42,41 @@ for(let i =0;i<document.querySelectorAll('button').length;i++){
     })
     
 }
+
+//the event parameter will let us to tap in to the event that triggered the function
+document.addEventListener('keypress',function(event){ //keyboard event listner is added in the whole document so that the whole document can hadle key press || first event is generated then the function gets triggered
+    console.log(event)
+    var keypressed = event.key;
+    switch (keypressed) {
+        case 'w':
+            var audio = new Audio('sounds/tom-1.mp3')
+            audio.play();
+            break;  
+        case 'a':
+            var audio = new Audio('sounds/tom-2.mp3')
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio('sounds/tom-3.mp3')
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio('sounds/tom-4.mp3')
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio('sounds/kick-bass.mp3')
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio('sounds/crash.mp3')
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio('sounds/snare.mp3')
+            audio.play();
+            break;
+        default: console.log(event.key)
+
+    }
+})
